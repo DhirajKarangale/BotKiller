140ab0aca1ea819dac294fc0dedc8c94c2a8445e
Dhiraj Karangale <dakarangale02@gmail.com>
2021-01-23 22:52:57 +0530

    Movement Animatation .
    Movement Animatation added , thrust is remaning to add

--- Bot Killer/Assets/Scripts/Player/Animatation.cs
+++ Bot Killer/Assets/Scripts/Player/Animatation.cs
@@ -8,28 +8,42 @@ public class Animatation : MonoBehaviour
     [SerializeField] Animator animator;
     private void Update()
     {
+             
+      
         float x = SimpleInput.GetAxis("Horizontal");
         float z = SimpleInput.GetAxis("Vertical");
+
+        if(weaponButton.shotting && ((x != 0) || (z != 0)))
+       {
+         animator.SetBool("OriginalPos",true);
+         animator.SetBool("Breath",false);
+         animator.SetBool("Reload",false);
+         animator.SetBool("Runing",false);
+       }
+     
 
         if((x==0) && (z==0) && (reloading == false) && (weaponButton.shotting == false))
         {
            animator.SetBool("Breath",true);
            animator.SetBool("Reload",false);
            animator.SetBool("Runing",false);
+           animator.SetBool("OriginalPos",false);
         }
-        else if((x != 0) || (z != 0) && (reloading == false) && (weaponButton.shotting == false))
+
+        if((weaponButton.shotting == false) && (reloading == false) && ((x != 0) || (z != 0)))
         {
            animator.SetBool("Breath",false);
            animator.SetBool("Reload",false);
            animator.SetBool("Runing",true);
+           animator.SetBool("OriginalPos",false);
         }
         
-       else if(reloading == true)
+       if(reloading == true)
        {
            animator.SetBool("Breath",false);
            animator.SetBool("Reload",true);
            animator.SetBool("Runing",false);
+           animator.SetBool("OriginalPos",false);
         }
-          
     }
 }
--- Bot Killer/Assets/Scripts/Player/PlayerMovement.cs
+++ Bot Killer/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,22 +7,44 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float gravity = -25f;
     [SerializeField] float groundDistance = 0.4f;
     [SerializeField] float jumpHeight = 5f;
+    [SerializeField] float thrustSpeed = 3000f;
 
     [Header("Points")]
     [SerializeField] CharacterController controller;
     [SerializeField] Transform groundCheck;
     [SerializeField] LayerMask groundMask;
+    [SerializeField] Animator animator;
 
+    public bool isThrust;
     private Vector3 gravityDownVelocity;
     private bool isGrounded;
-       
+    private Rigidbody rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }   
     
     private void Update()
     {
         Movement();
         Gravity();   
         Jump();
+        Animation();
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            Thrust();
+        }
+    }
 
+    public void PointerUp()
+    {
+      //  isThrust = false;
+    }
+
+    public void PointerDown()
+    {
+       // isThrust = true;
     }
 
     private void Movement()  // Adding left, right, forward, backword movement; 
@@ -58,11 +80,68 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    public void JumpButton()
+    private void JumpButton()
     {
        if(isGrounded)
         {
             gravityDownVelocity.y = Mathf.Sqrt(jumpHeight * (-2f) * gravity);
         }
     }
+
+    private void Animation()
+    {
+        float x = SimpleInput.GetAxis("Horizontal");
+        float z = SimpleInput.GetAxis("Vertical");
+
+        if(x > 0)
+        {
+            animator.SetBool("Left",true);
+            animator.SetBool("Right",false);
+            animator.SetBool("Forward",false);
+            animator.SetBool("Backward",false);
+            animator.SetBool("OriginalPos",false);
+        }
+        if(x < 0)
+        {
+            animator.SetBool("Left",false);
+            animator.SetBool("Right",true);
+            animator.SetBool("Forward",false);
+            animator.SetBool("Backward",false);
+            animator.SetBool("OriginalPos",false);
+        }
+
+        if(z < 0)
+        {
+            animator.SetBool("Left",false);
+            animator.SetBool("Right",false);
+            animator.SetBool("Forward",false);
+            animator.SetBool("Backward",true);
+            animator.SetBool("OriginalPos",false);
+        }
+
+        if(z > 0)
+        {
+            animator.SetBool("Left",false);
+            animator.SetBool("Right",false);
+            animator.SetBool("Forward",true);
+            animator.SetBool("Backward",false);
+            animator.SetBool("OriginalPos",false);
+        }
+       if((x==0) && (z==0))
+        {
+            animator.SetBool("Left",false);
+            animator.SetBool("Right",false);
+            animator.SetBool("Forward",false);
+            animator.SetBool("Backward",false);
+            animator.SetBool("OriginalPos",true);
+        }
+       
+    }
+     private void Thrust()
+        {
+            Debug.Log("Thrust Sucess");
+            //rigidBody.AddRelativeForce(Vector3.up * thrustSpeed * Time.deltaTime);
+            rigidBody.AddForce(0, thrustSpeed, 0, ForceMode.Impulse);
+        }
+
 }
