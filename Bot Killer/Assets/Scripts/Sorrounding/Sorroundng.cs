9ec16bf21154c8b9077a3c930a35e23b1d62980e
Dhiraj Karangale <dakarangale02@gmail.com>
2021-03-17 23:21:54 +0530

    Finally Completed

--- Bot Killer/Assets/Scripts/Sorrounding/Sorroundng.cs
+++ Bot Killer/Assets/Scripts/Sorrounding/Sorroundng.cs
@@ -2,32 +2,17 @@ using UnityEngine;
 
 public class Sorroundng : MonoBehaviour
 {
-   // private Follow_Attack enemy;
     [SerializeField] Player player;
     [SerializeField] Vector3 throwAmount;
-
-    private void Start()
-    {
-        //enemy = GameObject.FindGameObjectWithTag("Enemy").GetComponent<Follow_Attack>();
-    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Player")
         {
-            Debug.Log("Player collided to wall");
             if(player != null)
             {
                 player.transform.position = player.transform.position + throwAmount;
             }
         }
-       /* if (collision.gameObject.tag == "Enemy")
-        {
-          //  Debug.Log("Enemy collided to wall");
-           *//* if(enemy != null)
-            {
-                enemy.transform.position = enemy.transform.position + throwAmount;
-            }*//*
-        }*/
     }
 }
